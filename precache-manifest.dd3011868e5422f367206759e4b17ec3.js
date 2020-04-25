4824bc32592c51d8b2f18c8b707692d5021e7c67
sonish <sonishmzn95@gmail.com>
2020-04-25 22:02:09 +0545

    Updates

---
+++ precache-manifest.dd3011868e5422f367206759e4b17ec3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bee9c623124460cefeff90027e6c66e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "58543c7db87434a91e21",
+    "url": "/monsters-rolodex/static/css/main.bdf57c4a.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58543c7db87434a91e21",
+    "url": "/monsters-rolodex/static/js/main.fc9a580d.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
